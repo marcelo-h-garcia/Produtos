1963e85fdf8b59fd804bcd5caef80c0f94c0e7b8
Marcelo Helio Garcia <marceloph2@yahoo.com.br>
2021-04-08 21:23:53 -0300

    Exercicio fixação polimorfismo

---
+++ src/entities/UsedProduct.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UsedProduct extends Product {
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private Date manufactureDate;
+
+	public UsedProduct(String name, double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+
+	@Override
+	public String priceTag() {
+		return getName()
+				+ " (used) $ "
+				+ getPrice()
+				+ " (Manufature date: "
+				+ sdf.format(manufactureDate)
+				+ ")";
+	}			
+}
